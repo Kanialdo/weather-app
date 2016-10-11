1e0edebb8f1d66a78572254f06e6427a0af99786
Krystian Kaniowski <kanialdo@gmail.com>
2016-10-11 22:53:29 +0200

    Reset API keys and remove them from repository

--- app/src/main/java/pl/krystiankaniowski/weatherapp/data/openweathermap/OpenWeatherMapService.java
+++ app/src/main/java/pl/krystiankaniowski/weatherapp/data/openweathermap/OpenWeatherMapService.java
@@ -1,5 +1,6 @@
 package pl.krystiankaniowski.weatherapp.data.openweathermap;
 
+import pl.krystiankaniowski.weatherapp.BuildConfig;
 import pl.krystiankaniowski.weatherapp.data.openweathermap.model.WeatherData;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -9,7 +10,7 @@ public interface OpenWeatherMapService {
 
     String BASE_URL = "http://api.openweathermap.org/data/2.5/";
 
-    String API_KEY = "dacd3a49be6efee4c375a8465e244d69";
+    String API_KEY = BuildConfig.OPEN_WEATHER_MAP_API;
 
     @GET("weather")
     Observable<WeatherData> getCurrentData(@Query("id") int cityId, @Query("APPID") String api);
---
+++ app/src/main/java/pl/krystiankaniowski/weatherapp/data/places/GooglePlacesService.java
@@ -0,0 +1,13 @@
+package pl.krystiankaniowski.weatherapp.data.places;
+
+import pl.krystiankaniowski.weatherapp.BuildConfig;
+
+/**
+ * Created by kryst on 11.10.2016.
+ */
+
+public class GooglePlacesService {
+
+    String API_KEY = BuildConfig.GOOGLE_PLACES_API;
+
+}
